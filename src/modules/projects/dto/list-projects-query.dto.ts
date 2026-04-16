de73cbae8ea877355572f26f083b532e5f4f5ea1
BarreraJoel <joelmanuelbar@gmail.com>
2026-04-15 22:16:51 -0300

    fix: filter projects by category_id

--- src/modules/projects/dto/list-projects-query.dto.ts
+++ src/modules/projects/dto/list-projects-query.dto.ts
@@ -1,8 +1,9 @@
-import { IsEnum, IsNotEmpty, IsNumber, IsOptional, IsString, MinLength } from "class-validator";
+import { IsEnum, IsNotEmpty, IsNumber, IsOptional, IsString, Min, MinLength } from "class-validator";
 import { ProjectStatus, SortOptions } from "../../../types/enums";
 import { errorMessage } from "../../../helpers/messages";
 import { CategoryExists } from "../../../common/validators/category.validator";
 import { PaginateQueryDTO } from "../../../common/dto/paginate-query.dto";
+import { Type } from "class-transformer";
 
 export class ListProjectsQueryDTO extends PaginateQueryDTO {
 
@@ -11,6 +12,8 @@ export class ListProjectsQueryDTO extends PaginateQueryDTO {
     sort?: SortOptions;
 
     @CategoryExists({ message: errorMessage.invalid })
+    @Min(1, { message: errorMessage.min_numeric(1) })
+    @Type(() => Number)
     @IsNumber({}, { message: errorMessage.numeric })
     @IsOptional()
     category_id?: number;
